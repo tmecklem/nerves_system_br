f5a2e3260dd8b5282167822f906f12f11b3d2b60
Timothy Mecklem <timothy@mecklem.com>
2017-01-23 02:55:26 +0000

    Add fuser and allow unmodified mmeowlink package

--- package/python-mmeowlink/python-mmeowlink.mk
+++ package/python-mmeowlink/python-mmeowlink.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 PYTHON_MMEOWLINK_VERSION = 0.10.1
-PYTHON_MMEOWLINK_SOURCE = dev.tar.gz
-PYTHON_MMEOWLINK_SITE = https://github.com/tmecklem/mmeowlink/archive
+PYTHON_MMEOWLINK_SOURCE = mmeowlink-$(PYTHON_MMEOWLINK_VERSION).tar.gz
+PYTHON_MMEOWLINK_SITE = https://pypi.python.org/packages/59/d2/7a782361d58faddca37ece85d1fda0f7fb3aed129150957417c9bb0f4106
 PYTHON_MMEOWLINK_SETUP_TYPE = setuptools
 PYTHON_MMEOWLINK_LICENSE = GNU General Public License v3 (GPLv3)
 
